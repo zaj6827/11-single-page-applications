156fcd21f8b923beb192b8c72224d5677172b660
ZacharyJohnson <zaj6827@gmail.com>
2017-07-25 10:28:40 -0700

    pages not 404, but no articles rendered

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -2,5 +2,7 @@
 var app = app || {};
 
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
-
+page('/articles', articleController.articleBlast)
+page('/about', aboutController.aboutBlast)
 // TODO: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
+page()
